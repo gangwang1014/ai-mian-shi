ceb5ec6cc068bf55ff6f785b84dd0bded5c69cff
wanggwang <gangwang1014@163.com>
2025-07-30 18:44:47 +0800

    improvement: 权限管理增加添加接口

--- src/main/java/com/xxx/mianshiya/permission/service/PermissionService.java
+++ src/main/java/com/xxx/mianshiya/permission/service/PermissionService.java
@@ -1,5 +1,8 @@
 package com.xxx.mianshiya.permission.service;
 
+import com.xxx.mianshiya.permission.domain.entity.Permission;
+
 public interface PermissionService {
 
+    void add(Permission permission);
 }
